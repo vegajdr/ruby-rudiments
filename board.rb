58a67635344cf077f6457e72df69b732a77fde98
Vega Ramirez <jorgevp5@gmail.com>
2016-04-02 18:02:03 +0000

    end of lesson Day 13

--- board.rb
+++ board.rb
@@ -4,6 +4,8 @@ class Board
         @turn = 'X'
     end
     
+   
+    
     def play_x(i, j)
         if @turn == 'X'
             if @board[i][j] == ' '
@@ -34,25 +36,69 @@ class Board
         i = 0
         check = false
         while i < @board.length
-            @board[i][0] == 'X' && @board[i][1] == 'X' && @board[i][2] == 'X' or
-            @board[i][0] == 'O' && @board[i][1] == 'O' && @board[i][2] == 'O'
-            check = true
+            if
+                @board[i][0] == 'X' && @board[i][1] == 'X' && @board[i][2] == 'X' or
+                @board[i][0] == 'O' && @board[i][1] == 'O' && @board[i][2] == 'O'
+                check = true
+            end
             i += 1
         end
         return check
     end
     
-    def check_vertical(j)
+    def combined
+        i = 0
+        j = 0 
+        check = false
+        while i < @board.length # horizontal
+            if
+                @board[i][0] == 'X' && @board[i][1] == 'X' && @board[i][2] == 'X' or
+                @board[i][0] == 'O' && @board[i][1] == 'O' && @board[i][2] == 'O'
+                check = true
+            end
+            i += 1
+        end
+        
+        while j < @board.length # vertical
+            if
+                @board[0][j] == 'X' && @board[1][j] == 'X' && @board[2][j] == 'X' or
+                @board[0][j] == 'O' && @board[1][j] == 'O' && @board[2][j] == 'O'
+                check = true
+            end
+            j += 1
+        end
+        return check
+    end
+    
+    def check_vertical
+        j = 0
+        check = false
+        while j < @board.length
+            if
+                @board[0][j] == 'X' && @board[1][j] == 'X' && @board[2][j] == 'X' or
+                @board[0][j] == 'O' && @board[1][j] == 'O' && @board[2][j] == 'O'
+                check = true
+            end
+            j += 1
+        end
+        return check
     end
     
     def check_diag1
     end
     
+    def diag
+        (@board[0][0] == 'X' && @board[1][1] == 'X' && @board[2][2] == 'X' or
+        @board[0][2] == 'X' && @board[1][1] == 'X' && @board[2][0] == 'X') or
+        (@board[0][0] == 'O' && @board[1][1] == 'O' && @board[2][2] == 'O' or
+        @board[0][2] == 'O' && @board[1][1] == 'O' && @board[2][0] == 'O')
+                
+    end
+    
     def check_diag2
     end
     
-    def win?
-    end
+    
     
     def to_s
         i = 0
@@ -63,4 +109,29 @@ class Board
         end
         return s
     end
+    
+    def win? # Not working!
+        if @board.combined == true || @board.diag == true
+            puts "You win!"
+        end
+        
+    end
 end
+
+board = Board.new
+board.play_x(0,0)
+board.play_o(1,0)
+board.play_x(0,1)
+board.play_o(2,0)
+board.play_x(0,2)
+
+
+puts board.to_s
+
+puts board.win?
+#puts board.check_horizontal
+#puts board.check_vertical
+#puts board.diag
+#puts board.combined
+#puts board.check_diag1
+#puts board.check_diag2
